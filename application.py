a6972165663793c8089bfd88a768d5386a551e15
princekumar1402 <princek20012@gmail.com>
2026-02-01 01:35:10 +0530

    Fix

--- application.py
+++ application.py
@@ -32,4 +32,4 @@ def predict():
     return render_template("home.html", results=round(preds[0], 2))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", debug=True)
